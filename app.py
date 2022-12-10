e099e0c36bca4747b6a864fc2957437432a236b4
Venkatesh Murugadas <venkateshdas.me@gmail.com>
2022-12-10 01:43:09 +0100

    Change app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from io import BytesIO
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
+    # Load the model
     model = whisper.load_model("base")
 
 # Inference is ran for every server call
@@ -25,9 +25,27 @@ def inference(model_inputs:dict) -> dict:
     with open('input.mp3','wb') as file:
         file.write(mp3Bytes.getbuffer())
     
-    # Run the model
-    result = model.transcribe("input.mp3")
-    output = {"text":result["text"]}
+    audio = whisper.load_audio("input.mp3")
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    lang = max(probs, key=probs.get)
+    print(f"Detected language: {lang}")
+
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
+
+    # print the recognized text
+    output = {"text":result["text"], "lang":lang}
     os.remove("input.mp3")
+    
+    # # Run the model
+    # result = model.transcribe("input.mp3")
+    # output = {"text":result["text"]}
+    # os.remove("input.mp3")
     # Return the results as a dictionary
     return output
